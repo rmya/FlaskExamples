0de0744f7d2e80d140c9b174a307f2ca22b3de67
rmya <tanrumeysa93@gmail.com>
2021-02-04 00:21:39 +0300

    Todo App projesi oluşturuldu app.py fixed

---
+++ ToDo-App/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  title = db.Column(db.String(100))
+  complete = db.Column(db.Boolean)
+
+@app.route('/')
+def home():
+  todo_list = Todo.query.all()
+  return render_template("base.html", todo_list=todo_list)
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
