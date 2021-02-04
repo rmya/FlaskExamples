8cf28319f838764a6b3612f87e43ed55d27ad0b2
rmya <tanrumeysa93@gmail.com>
2021-02-04 21:58:33 +0300

    db sorunu çözüldü. Fonksiyonlar çalışıyor.

--- ToDo-App/app.py
+++ ToDo-App/app.py
@@ -3,42 +3,48 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite'
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
-  id = db.Column(db.Integer, primary_key = True)
-  title = db.Column(db.String(100))
-  complete = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  todo_list = Todo.query.all()
-  return render_template("base.html", todo_list=todo_list)
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
 
-@app.route("/add", methods = ["POST"])
+
+@app.route("/add", methods=["POST"])
 def add():
-  title = request.form.get("title")
-  new_todo = Todo(title=title, complete = False)
-  db.session.add(new_todo)
-  db.session.commit()
-  return redirect(url_for("home"))
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-  todo = Todo.title.query.filter_by(id=todo_id).first()
-  todo.complete = not todo.complete
-  db.session.commit()
-  return redirect(url_for("home"))
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
 
-@app.route("/delete/<int:todo_id")
+
+@app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-  todo = Todo.query.filter_by(id = todo_id).first()
-  db.session.commit()
-  return redirect(url_for("home"))
-
-
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
