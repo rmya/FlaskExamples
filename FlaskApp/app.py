6a2f08e9a6a2c28d7b5b0821a371e8aced528692
rmya <tanrumeysa93@gmail.com>
2021-02-18 21:49:33 +0300

    part1 complate

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
   return render_template('home.html')
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
-if __name__ == '__main':
+@app.route('/articles')
+def articles():
+  return render_template('articles.html', articles=Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+  return render_template('article.html', id=id)
+
+
+
+
+if __name__ == "__main__":
   app.run(debug=True)   #debug = True ile web sitesinden syntax gibi hataları almamızı sağlıyor ve değişiklikler sırasında serverı yeniden başlatmayı önler.
---
+++ FlaskApp/data.py
@@ -0,0 +1,26 @@
+def Articles():
+  articles = [
+    {
+      'id' : 1,
+      'title' : 'Article One',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla tempore error fuga repellendus! Id nemo vero eum corrupti, dolorum saepe, qui nobis aspernatur accusantium tempora sed animi iste dignissimos.',
+      'author' : 'Rumeysa Tan',
+      'create_date' : '18-02-2021'
+    },
+    {
+      'id' : 2,
+      'title' : 'Article Two',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla tempore error fuga repellendus! Id nemo vero eum corrupti, dolorum saepe, qui nobis aspernatur accusantium tempora sed animi iste dignissimos.',
+      'author' : 'Ruken Sancar',
+      'create_date' : '18-02-2021'
+    },
+    {
+      'id' : 3,
+      'title' : 'Article Three',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla tempore error fuga repellendus! Id nemo vero eum corrupti, dolorum saepe, qui nobis aspernatur accusantium tempora sed animi iste dignissimos.',
+      'author' : 'Kübra Durak',
+      'create_date' : '18-02-2021'
+    }
+  ]
+
+  return articles
