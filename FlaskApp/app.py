1e541099cfa4c3a61a616d0fe595124987e52aa3
rmya <tanrumeysa93@gmail.com>
2021-02-19 22:56:34 +0300

    mySQL :(

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-#from passlib import sha256_crypt
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+#config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MySQL
+mysql = MySQL(app)    #app içine tanımladığımız config bilgilerini mysql değişkenimize bağlıyoruz.
+
 
 Articles = Articles()
 
@@ -38,7 +48,24 @@ class RegisterForm(Form):
 def register():
   form = RegisterForm(request.form)
   if request.method == 'POST' and form.validate():
-    pass
+    name = form.name.data
+    email = form.email.data
+    username = form.username.data
+    password = sha256_crypt.encrypt(str(form.password.data))
+
+    #create cursor
+    cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)" , (name, email, username, password))
+
+    #commit to DB
+    mysql.connection.commit()    #Yapılan değişikleri kaydetmek ve veritabında uygulamak için gerekli olan commit fonksiyonu. Eğer bu kısmı yazmazsanız değişiklikleriniz veritabanınızda uygulanmayacaktır.
+
+    #close connection
+    cursor.close()
+
+    flash('You are now registered and can log in', 'success')
+
+    return redirect(url_for('login'))
 
   return render_template('register.html', form=form)
 
@@ -48,4 +75,5 @@ def register():
 
 
 if __name__ == "__main__":
+  app.secret_key = 'secret123'
   app.run(debug=True)   #debug = True ile web sitesinden syntax gibi hataları almamızı sağlıyor ve değişiklikler sırasında serverı yeniden başlatmayı önler.
